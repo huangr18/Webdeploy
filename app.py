ac65ac02395475d3f545412987c7f738ab049eef
huangr18 <huangr18@miamioh.edu>
2023-06-22 15:59:15 -0400

    functioning web

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
 from flask import Flask, render_template, jsonify
+from database import load_video_from_db, load_user_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Sales',
-        'location': 'Scranton',
-        'name': 'Jim'
-    },
-    {
-        'id': 2,
-        'title': 'Actor',
-        'location': 'England',
-        'name': 'Emily'
-    },
-    {
-        'id': 3,
-        'title': 'Receptant',
-        'location': 'Scranton',
-        'name': 'Pam'
-    }
-]
+
+
 
 @app.route("/")
 def home():
-    return render_template('home.html', jobs=JOBS, company_name='Dunder Mifflin')
+    jobs = load_video_from_db()
+    return render_template('home.html', jobs=jobs, company_name='Dunder Mifflin')
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_video_from_db()
+    return jsonify(jobs)
+
+@app.route("/user/<id>")
+def show_user(id):
+    user = load_user_from_db(id)
+    if not user:
+        return "Not found", 404
+    return render_template('upload.html', user=user)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
---
+++ database.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_connection_string = os.getenv('DB_CONNECTION_STRING')
+
+engine = create_engine(db_connection_string)
+
+
+def load_video_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from test_table"))
+
+        result_all = []
+        for row in result.all():
+            result_all.append(dict(row))
+        return result_all
+    
+def load_user_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from test_table WHERE id = :val"), val=id)
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
+
+
+
+
+
+
+
+
+
+
+
+    # print(result_all)
+    # print(result.all())
+    # print(type(result.all()))
+    # result_all = result.all()
+    
+    # first = result_all[1]
+    # print(type(first))
